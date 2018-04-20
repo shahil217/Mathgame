43cc1162a65781e7cc99f5dbe9ce7e634a341ce4
shahil217 <38421128+shahil217@users.noreply.github.com>
2018-04-20 18:11:14 +1000

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,30 @@
+public class Main{
+    int num1;
+    int num2;
+    String operation;
+
+    public Main(int num1, int num2){
+        this.num1 = num1;
+        this.num2 = num2;
+        this.operation = operation;
+
+    }
+    public int add(){
+        return num1 + num2;
+    }
+
+    public int multiply(){
+        return num1 * num2;
+    }
+
+    public int minus(){
+        return num1 - num2;
+        }
+
+        public int divide() {
+            return num1 / num2;
+        }
+
+
+}
+
